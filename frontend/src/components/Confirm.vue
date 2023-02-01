00a387ee47b0f0fb71407ef98a17ac86da11df10
Blackrose-1337 <21s6627@bztf.ch>
2023-02-01 14:51:57 +0100

    add: confirm

--- frontend/src/components/Confirm.vue
+++ frontend/src/components/Confirm.vue
@@ -3,25 +3,7 @@ import { computed, onMounted } from "vue";
 import type { Auswertung } from "@/stores/interfaces";
 import { useEvaluationStore } from "@/stores/evaluation.ts";
 
-const evaluationstore = useEvaluationStore();
-const auswertung = computed(() => evaluationstore.auswertung) as Auswertung;
-const columns = [
-    {
-        name: "surname",
-        required: true,
-        label: "Nachname",
-        align: "left",
-        field: "surname",
-        format: (val: any) => `${val}`,
-        sortable: true,
-    },
-    { name: "name", align: "left", label: "Vorname", field: "name", sortable: true },
-    { name: "mail", label: "E-mail", field: "mail", sortable: true },
-    { name: "value", label: "Gesamtpunkte", field: "value", sortable: true },
-];
-onMounted(() => {
-    evaluationstore.getAnalysis();
-});
+onMounted(() => {});
 </script>
 <template>
     <div class="q-pa-md">
